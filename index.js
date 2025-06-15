68508dff4185f7583fac664176f49c7c979e73c4
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2025-06-15 10:43:36 +0100

    just commit

--- index.js
+++ index.js
@@ -9,11 +9,6 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "10mb" }));
 
-app.get("/", (req, res) => {
-  console.log("Server is running");
-  res.send("this endpoint is for testing the server!");
-});
-
 app.post("/my-n8n-endpoint", async (req, res) => {
   const response = await fetch(
     "https://n8n.srv805351.hstgr.cloud/webhook-test/real-estate-upload",
@@ -26,6 +21,11 @@ app.post("/my-n8n-endpoint", async (req, res) => {
   const data = await response.text(); // or .json()
   console.log("Response from n8n:", data);
   res.status(response.status).send(data);
+});
+
+app.get("/", (req, res) => {
+  console.log("Server is running");
+  res.send("this endpoint is for testing the server!");
 });
 
 app.listen(PORT, (err) => {
